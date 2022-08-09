083f1ecfa79ef92901c90d81d8a6df0830b9759c
Makktu <johnmcna@gmail.com>
2022-08-09 19:43:55 +0100

    webpack struggles...

--- src/getLocation.js
+++ src/getLocation.js
@@ -1,10 +1,14 @@
 export default async function getUserLocation() {
     console.log('linked');
+
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition;
+    // } else {
+    //     x.innerHTML = 'Geolocation is not supported by this browser.';
+    // }
 }
 
-// if (navigator.geolocation) {
-//     console.log(
-//         navigator.geolocation.getCurrentPosition().position.coords.latitude
-//     );
-// } else {
-//     x.innerHTML = 'Geolocation is not supported by this browser.';
+function position(pos) {
+    console.log('✅', pos);
+    return `${pos.coords.latitude + ' ' + pos.coords.longitude}`;
+}
